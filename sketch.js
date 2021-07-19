55cbcfa85565044c4c49ff7d1fa77b21a86cf8e4
krishtanwar844 <87520242+krishtanwar844@users.noreply.github.com>
2021-07-19 19:03:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(RIGHT_ARROW)){
+  background("red")
+}
+if (keyIsDown(LEFT_ARROW)){
+  background("blue")
+}
+if (keyIsDown(DOWN_ARROW)){
+  background("white")
+}
+if (keyIsDown(UP_ARROW)){
+  background("green")
+}
+}
+
+
+
+
